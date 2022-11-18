5eb8ddead4d02bcda688f35184aa977aed0f8d0e
unexpectedhaki <thomas.pritchard20@gmail.com>
2022-11-17 19:35:18 -0500

    initial commit with functional Javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordLength = 12
+let firstPasswordEl = document.getElementById("password-one")
+let secondPasswordEl = document.getElementById("password-two")
+
+function generatePassword() {
+    firstPasswordEl.textContent = ""
+    secondPasswordEl.textContent = ""
+    for (let i = 0; i < passwordLength; i++) {
+        firstPassword = Math.floor( Math.random() * characters.length )
+        firstPasswordEl.textContent += characters[firstPassword]
+
+        secondPassword = Math.floor( Math.random() * characters.length )
+        secondPasswordEl.textContent += characters[secondPassword]
+    }
+}
