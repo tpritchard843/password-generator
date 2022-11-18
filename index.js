954e03156b604189b5f1422d0695a27af56acf5d
unexpectedhaki <thomas.pritchard20@gmail.com>
2022-11-18 11:02:02 -0500

    Refactored JavaScript  and modified CSS

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordLength = 12
+let passwordLength = 15
 let firstPasswordEl = document.getElementById("password-one")
 let secondPasswordEl = document.getElementById("password-two")
 
 function generatePassword() {
-    firstPasswordEl.textContent = ""
-    secondPasswordEl.textContent = ""
+    let password = ""
     for (let i = 0; i < passwordLength; i++) {
-        firstPassword = Math.floor( Math.random() * characters.length )
-        firstPasswordEl.textContent += characters[firstPassword]
-
-        secondPassword = Math.floor( Math.random() * characters.length )
-        secondPasswordEl.textContent += characters[secondPassword]
+        randomCharacter = Math.floor( Math.random() * characters.length )
+        password += characters[randomCharacter]
     }
+    return password
+}
+
+function renderPasswords() {
+    firstPasswordEl.textContent = generatePassword()
+    secondPasswordEl.textContent = generatePassword()
+}
+
+function copyPassword(password) {
+    navigator.clipboard.writeText(password.textContent)
+    alert("Copied password: " + password.textContent)
 }
